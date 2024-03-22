6721792ba32061f4cbb052408a354f5cd4fb5b3a
Rehab Adel <rehabadel1212@gmail.com>
2024-03-22 02:57:28 +0200

    Add Validations

--- RentasticBackEnd/DTO/ReservationDTO.cs
+++ RentasticBackEnd/DTO/ReservationDTO.cs
@@ -1,4 +1,7 @@
-﻿namespace RentasticBackEnd.DTO
+﻿using FluentValidation;
+using System;
+
+namespace RentasticBackEnd.DTO
 {
     public class ReservationDTO
     {
@@ -8,5 +11,19 @@
         public DateTime StartRentTime { get; set; }
         public DateTime EndRentDate { get; set; }
         public double TotalPrice { get; set; }
+    }
+
+    public class CarReservationValidator : AbstractValidator<ReservationDTO>
+    {
+        public CarReservationValidator()
+        {
+            RuleFor(x => x.UserSsn).NotEmpty().WithMessage("User SSN is required.");
+            RuleFor(x => x.CarId).NotEmpty().WithMessage("Car ID is required.");
+            RuleFor(x => x.StartRentTime).NotEmpty().WithMessage("Start rent time is required.")
+                .GreaterThanOrEqualTo(DateTime.Now).WithMessage("Start rent time should not be in the past.");
+            RuleFor(x => x.EndRentDate).NotEmpty().WithMessage("End rent date is required.")
+                .GreaterThan(x => x.StartRentTime).WithMessage("End rent date should be after start rent time.");
+            RuleFor(x => x.TotalPrice).GreaterThan(0).WithMessage("Total price should be greater than 0.");
+        }
     }
 }
